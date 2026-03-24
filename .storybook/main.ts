a13ddd0efbe53bff13157c0f95f12d62bd327987
wacky-sjh <sjh@wacky.kr>
2026-03-24 16:16:27 +0900

    빌드 에러 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,16 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "staticDirs": ["../output"],
+  "staticDirs": [path.resolve(__dirname, "../output")],
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
